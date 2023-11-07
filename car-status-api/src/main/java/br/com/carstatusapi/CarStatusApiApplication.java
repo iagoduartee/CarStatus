71b3d9c8be054a3f0e579c0cea0b447ce940b9b2
Iago <iagodduarte@gmail.com>
2023-11-07 20:16:53 -0300

    setup inicial do projeto

---
+++ car-status-api/src/main/java/br/com/carstatusapi/CarStatusApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carstatusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStatusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStatusApiApplication.class, args);
+	}
+
+}
