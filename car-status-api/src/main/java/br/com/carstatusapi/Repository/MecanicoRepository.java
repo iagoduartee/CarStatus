61e9656fd13ed1e048969717f10ec9cab17c1b07
Iago <iagodduarte@gmail.com>
2023-11-13 20:02:39 -0300

    feat(Mecanico): Adicionar Entidade, Repositório e DTO para Mecanico.

---
+++ car-status-api/src/main/java/br/com/carstatusapi/Repository/MecanicoRepository.java
@@ -0,0 +1,18 @@
+package br.com.carstatusapi.Repository;
+
+import br.com.carstatusapi.Entity.MecanicoEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MecanicoRepository extends CrudRepository<MecanicoEntity, Long> {
+    MecanicoEntity save(MecanicoEntity pessoa);
+
+    Optional<MecanicoEntity> findById(Long id);
+
+    List<MecanicoEntity> findAll();
+}
+
