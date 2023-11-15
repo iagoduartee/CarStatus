efb2693a7a4694b295eade03b43270868d0205ef
Iago <iagodduarte@gmail.com>
2023-11-15 00:22:45 -0300

    feat(Cliente): Adicionar Entidade e DTO.

---
+++ car-status-api/src/main/java/br/com/carstatusapi/DTO/ClienteDTO.java
@@ -0,0 +1,24 @@
+package br.com.carstatusapi.DTO;
+
+import java.util.List;
+
+public class ClienteDTO extends PessoaDTO {
+    List<CarroDTO> carros;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Long id, String nome, String cpf, List<CarroDTO> carros) {
+        super(id, nome, cpf);
+        this.carros = carros;
+    }
+
+    public List<CarroDTO> getCarros() {
+        return carros;
+    }
+
+    public void setCarros(List<CarroDTO> carros) {
+        this.carros = carros;
+    }
+}
+
---
+++ car-status-api/src/main/java/br/com/carstatusapi/Entity/ClienteEntity.java
@@ -0,0 +1,40 @@
+package br.com.carstatusapi.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class ClienteEntity extends PessoaEntity{
+
+    @OneToMany( mappedBy = "dono")
+    private List<CarroEntity> carros;
+
+    @OneToMany( mappedBy = "cliente")
+    private List<ManutencaoEntity> manutencoes;
+
+    public ClienteEntity() {
+    }
+
+    public ClienteEntity(Long id, String nome, String cpf, List<CarroEntity> carros, List<ManutencaoEntity> manutencoes) {
+        super(id, nome, cpf);
+        this.carros = carros;
+        this.manutencoes = manutencoes;
+    }
+
+    public List<CarroEntity> getCarros() {
+        return carros;
+    }
+
+    public void setCarros(List<CarroEntity> carros) {
+        this.carros = carros;
+    }
+
+    public List<ManutencaoEntity> getManutencoes() {
+        return manutencoes;
+    }
+
+    public void setManutencoes(List<ManutencaoEntity> manutencoes) {
+        this.manutencoes = manutencoes;
+    }
+}
